db93216beb877045ca62c9336923efb28aa53a84
multifacs <kiwi0tails@gmail.com>
2025-04-12 09:42:56 +0300

    gh action

--- models/index.ts
+++ models/index.ts
@@ -3,10 +3,14 @@ import path from "path";
 
 const DB_PATH = process.env.DB_PATH as string;
 
+const filepath = path.resolve(__dirname, DB_PATH);
+
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: path.resolve(__dirname, DB_PATH),
+  storage: filepath,
 });
+
+console.log("DB file created at", filepath);
 
 import { User } from "./user.model";
 
