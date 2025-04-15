a04cce12cc7b212f117b60dd591aee835f0af1b4
multifacs <kiwi0tails@gmail.com>
2025-04-15 20:03:31 +0300

    stuff

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from "body-parser";
+
 import userRoutes from './routes/user.routes';
+import testRoutes from './routes/test.routes';
 
 const app = express();
 
@@ -12,5 +14,6 @@ app.use(bodyParser.json());
 
 // Подключаем роуты
 app.use('/api/users', userRoutes);
+app.use('/api/results', testRoutes);
 
 export default app;
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import app from './app'
+import app from "./app";
+const MODE = process.env.MODE as string;
 
 const port = 3000;
 
 // Create Sequelize instance
 import { sequelize } from "./models/index";
 // Sync models with database
-sequelize.sync();
+console.log(`Starting in ${MODE} mode`)
+// if (MODE === "DEV") sequelize.sync({ force: true });
+if (MODE === "PROD") sequelize.sync({ alter: true });
 
 // Start server
 app.listen(port, () => {
